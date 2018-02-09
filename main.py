f72dc602367bf127ad1560e21d3e2289bd5ffaac
Ender <gawlik.grzegorz@yahoo.com>
2018-02-09 02:07:44 +0100

    New operation - multiplication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Hey, it's my first project in Python
-# It's just a simple calculator that adds and subtracts two numbers.
+# It's just a simple calculator that adds, subtracts and multiplicates two numbers.
 # Enjoy! =)
 
 from sys import exit
@@ -14,7 +14,8 @@ second_number = int(input("Enter second number: "))
 operation = int(input("""Choose an operation:
 1 <- add numbers
 2 <- substract second number from first
-3 <- substract first number from second"""))
+3 <- substract first number from second
+4 <- multiplicate numbers"""))
 
 if operation == 1:
     result = first_number + second_number
@@ -22,6 +23,8 @@ elif operation == 2:
     result = first_number - second_number
 elif operation == 3:
     result = second_number - first_number
+elif operation == 4:
+    result = first_number * second_number
 else:
     print("I don't recognise the operation!")
     exit()
